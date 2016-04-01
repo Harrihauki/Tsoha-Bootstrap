73e9034bb64efaff9c7d4c95dbce4a4d7fc2b117
Myllyaho Lalli S <lallimyl@da5-cs-b221-28.helsinki.fi>
2016-04-01 16:57:03 +0300

    html-suunnitelmat ja niiden linkit Readmehen

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,42 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('suunnitelmat/etusivu.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function team_list() {
+        View::make('suunnitelmat/team_list.html');
+    }
+    
+    public static function esittely() {
+        View::make('suunnitelmat/esittely.html');
+    }
+    
+    public static function ottelu() {
+        View::make('suunnitelmat/ottelu.html');
+    }
+
+    public static function team_add() {
+        View::make('suunnitelmat/team_add.html');
+    }
+    
+    public static function match_add() {
+        View::make('suunnitelmat/match_add.html');
+    }
+    
+    public static function team_edit() {
+        View::make('suunnitelmat/team_edit.html');
+    }
+    
+    public static function match_edit() {
+        View::make('suunnitelmat/match_edit.html');
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,37 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/suunnitelmat/team', function() {
+    HelloWorldController::team_list();
+});
+
+$routes->get('/suunnitelmat/esittely', function() {
+    HelloWorldController::esittely();
+});
+
+$routes->get('/suunnitelmat/ottelu', function() {
+    HelloWorldController::ottelu();
+});
+
+$routes->get('/suunnitelmat/team_add', function() {
+    HelloWorldController::team_add();
+});
+
+$routes->get('/suunnitelmat/match_add', function() {
+    HelloWorldController::match_add();
+});
+
+$routes->get('/suunnitelmat/team_edit', function() {
+    HelloWorldController::team_edit();
+});
+
+$routes->get('/suunnitelmat/match_edit', function() {
+    HelloWorldController::match_edit();
+});
